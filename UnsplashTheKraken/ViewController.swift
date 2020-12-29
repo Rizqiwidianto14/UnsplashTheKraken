5a2a569f35eca4a6d3bf95492614d51608e808b4
Rizqi Imam Gilang <widiantorizqi@gmail.com>
2020-12-29 14:37:28 +0700

    first commit

---
+++ UnsplashTheKraken/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnsplashTheKraken
+//
+//  Created by Rizqi Imam Gilang Widianto on 29/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
